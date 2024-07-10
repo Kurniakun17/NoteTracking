6c1e27e4072639d4f4dc15c03a9480555ab0031a
Kurniakun17 <kurniakharisma17@gmail.com>
2024-07-10 13:11:06 +0700

    Initial Commit

---
+++ NotesHabit/NotesHabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesHabitApp.swift
+//  NotesHabit
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 10/07/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesHabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
